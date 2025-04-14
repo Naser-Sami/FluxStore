d45b649d61980700431175ec63a74a042ab9aeb8
Naser Sami <naser_ebedo@icloud.com>
2025-04-14 13:54:51 +0300

    Updated

--- lib/features/_features.dart
+++ lib/features/_features.dart
@@ -1,4 +1,4 @@
-export '_customer/_initial_screens/_initial_screens.dart';
-export '_customer/authentication/_authentication.dart';
-export '_customer/bottom_navigation_bar/_bottom_navigation_bar.dart';
+export '_initial_screens/_initial_screens.dart';
+export 'authentication/_authentication.dart';
+export 'bottom_navigation_bar/_bottom_navigation_bar.dart';
 export '_new_feature/_new_feature.dart';

3c8fbbcf6d261fed30c3f3a6ee074a94bcf0e3c3
Naser Sami <naser_ebedo@icloud.com>
2025-04-18 00:46:09 +0300

    Product repository

---
+++ lib/features/product/data/repo_impl/product_repository_impl.dart
@@ -0,0 +1,134 @@
+import 'package:dartz/dartz.dart';
+
+import '/core/_core.dart' show Failure;
+import '/features/_features.dart'
+    show
+        IProductRepository,
+        Product,
+        ProductMapper,
+        ProductDetails,
+        ProductDetailsMapper,
+        AddProductParams,
+        UpdateProductParams,
+        AddProductReviewParams,
+        IProductRemoteDataSource;
+
+class ProductRepositoryImpl implements IProductRepository {
+  final IProductRemoteDataSource remoteDataSource;
+  ProductRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<Failure<String>, Product>> addProduct(
+    AddProductParams p,
+  ) async {
+    try {
+      final result = await remoteDataSource.addProduct(p);
+      final resultEntity = ProductMapper.toDomain(result);
+
+      return Right(resultEntity);
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, String>> addProductReview(
+    AddProductReviewParams p,
+  ) async {
+    try {
+      final result = await remoteDataSource.addProductReview(p);
+
+      if (result == 'Success') {
+        return Right(result);
+      } else {
+        return Left(Failure(statusCode: 500, error: result));
+      }
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, String>> deleteProduct(String id) async {
+    try {
+      final result = await remoteDataSource.deleteProduct(id);
+
+      if (result == 'Product deleted successfully') {
+        return Right(result);
+      } else {
+        return Left(Failure(statusCode: 500, error: result));
+      }
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, Product>> getProductById(String id) async {
+    try {
+      final result = await remoteDataSource.getProductById(id);
+      final resultEntity = ProductMapper.toDomain(result);
+
+      return Right(resultEntity);
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, List<Product>>> getProducts() async {
+    try {
+      final result = await remoteDataSource.getProducts();
+      final resultEntity = result.map(ProductMapper.toDomain).toList();
+
+      return Right(resultEntity);
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, ProductDetails>> getProductsDetailsById(
+    String id,
+  ) async {
+    try {
+      final result = await remoteDataSource.getProductsDetailsById(id);
+      final resultEntity = ProductDetailsMapper.toDomain(result);
+
+      return Right(resultEntity);
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure<String>, String>> updateProduct(
+    UpdateProductParams p,
+  ) async {
+    try {
+      final result = await remoteDataSource.updateProduct(p);
+
+      if (result == 'Product updated successfully') {
+        return Right(result);
+      } else {
+        return Left(Failure(statusCode: 500, error: result));
+      }
+    } on Failure catch (e) {
+      return Left(Failure(statusCode: e.statusCode, error: e.error));
+    } catch (e) {
+      return Left(Failure(statusCode: 500, error: e.toString()));
+    }
+  }
+}
---
+++ lib/features/product/domain/repo/product_repository.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+
+import '/core/_core.dart' show Failure;
+import '/features/_features.dart'
+    show
+        Product,
+        ProductDetails,
+        AddProductParams,
+        UpdateProductParams,
+        AddProductReviewParams;
+
+abstract class IProductRepository {
+  Future<Either<Failure<String>, List<Product>>> getProducts();
+  Future<Either<Failure<String>, Product>> getProductById(String id);
+  Future<Either<Failure<String>, Product>> addProduct(AddProductParams p);
+  Future<Either<Failure<String>, String>> updateProduct(UpdateProductParams p);
+  Future<Either<Failure<String>, String>> deleteProduct(String id);
+  Future<Either<Failure<String>, ProductDetails>> getProductsDetailsById(
+    String id,
+  );
+  Future<Either<Failure<String>, String>> addProductReview(
+    AddProductReviewParams p,
+  );
+}

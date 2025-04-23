11a1db9d4e58c898f5af4f1ef7f8cfc0f9e3b0f2
Naser Sami <naser_ebedo@icloud.com>
2025-04-23 10:22:41 +0300

    Updated

--- lib/features/navigation/bottom_navigation_bar.dart
+++ lib/features/navigation/bottom_navigation_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 import '/config/_config.dart'
     show

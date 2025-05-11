7c8b552060ccf916001fd577842a2c885d05bf84
Naser Sami <naser_ebedo@icloud.com>
2025-05-11 22:32:25 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ Future<void> main() async {
   runApp(MultiBlocProvider(providers: providers, child: const MyApp()));
 }
 
-//
 // Easy Localization Generated Commands
 // dart run easy_localization:generate --source-dir ./assets/translations
 // dart run easy_localization:generate --source-dir ./assets/translations -f keys -o locale_keys.g.dart
